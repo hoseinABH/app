1aec5df9b4367f3adcd2438c6bd5726cb6f49a84
Hosein <hosein.abolhasani1998@gmail.com>
2023-02-28 09:15:05 +0330

    chore:init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
+import { exportComponentAsPNG } from 'react-component-export-image';
 
 function App() {
+  const testRef = useRef();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p ref={testRef}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -18,6 +21,22 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <button
+        style={{ margin: '30px' }}
+        onClick={() => {
+          console.log('test');
+          exportComponentAsPNG(testRef, {
+            html2CanvasOptions: {
+              // onclone: (clonedDoc) => {
+              //   clonedDoc.getElementById('legend').style.visibility = 'visible';
+              // },
+            },
+          });
+        }}
+      >
+        Export As PNG
+      </button>
     </div>
   );
 }
